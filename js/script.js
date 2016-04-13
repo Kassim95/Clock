65ce51c31e6b027f78b8f4027cfe76555dcc9c11
keyanbagherin <keyanbagheri@gmail.com>
2016-04-13 14:46:52 -0400

    adding js to update time in real time

--- js/script.js
+++ js/script.js
@@ -1,3 +1,18 @@
 $(document).ready(function(){
-	
+
+	var nextTick = setInterval(function(){
+
+		var time = new Date();
+		var hr = time.getHours();
+		var min = time.getMinutes();
+		var sec = time.getSeconds();
+		var ms = time.getMilliseconds();
+
+		$('#hr').text(hr);
+		$('#min').text(min);
+		$('#sec').text(sec);
+		$('#ms').text(ms);
+
+	}, 1);
+
 });
