52c520632960cbd3c13a415f6a59cab6fd37dfcc
Kassim Ouattara <ouat.kassim@gmail.com>
2016-04-14 10:00:28 -0400

    adding comments

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-
+// setting the clock 
 	var nextTick = setInterval(function(){
 
 		var time = new Date();
@@ -22,14 +22,14 @@ $(document).ready(function(){
 		}
 	});
 	var colors = ["#CCCCCC","#333333","#990099"];  
-
+// background color changes every 5 minutes randomly
 	window.setInterval(function(){
 
 		var rand = Math.floor(Math.random()*colors.length);           
 		$('body').css({"background-color": colors[rand]});
 
 	}, 300000);
-
+// "What time is it" color changes every hour randomly
 	window.setInterval(function(){
 		var rand = Math.floor(Math.random()*colors.length);           
 		$('h1').css({"color": colors[rand]});	
